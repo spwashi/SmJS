fcd298a53d131495d2d455c142edccb5550a9759
Sam Washington <spwashi2@illinois.edu>
2017-06-09 11:29:28 -0500

    SmJS-53 Add "Unique" status to the Model's PropertyMetaContainer for each different Property we identify as being part of a unique key

--- _deploy/tests/entities/ModelTest.js
+++ _deploy/tests/entities/ModelTest.js
@@ -82,25 +82,30 @@ describe('Model', () => {
     });
     
     it('Can register Unique properties', done => {
+        /** @type property2 {Property}  */
+        let property2;
         const _model_name     = 'unique_test_mn';
         const _property_name  = 'unique_test_pn';
         const _property_name2 = 'unique_test_pn2';
         const model_name      = `[Model]${_model_name}`;
-        const model           = new Model(_model_name, {
+    
+        new Model(_model_name, {
             properties: {
                 [_property_name]:  {primary: true, unique: true},
                 [_property_name2]: {unique: true},
             }
         });
-        /** @type property2 {Property}  */
-        let property2;
+    
         Std.resolve(`${model_name}|${_property_name2}`)
            .then(i => [, property2] = i)
            .then(i => Std.resolve(model_name))
            .then(i => {
                let [e, model]     = i;
                const uniqueKeySet = model.propertyMeta.getUniqueKeySet(property2);
-               const message      = uniqueKeySet ? (uniqueKeySet.get('unique_key').size === 2 ? null : 'Missing one property') : 'Could not successfully incorporate unique key';
+               const message      =
+                       !uniqueKeySet
+                           ? 'Could not successfully incorporate unique key'
+                           : (uniqueKeySet.get('unique_key').size < 2 ? 'Missing one property' : null);
                done(message);
            });
     });

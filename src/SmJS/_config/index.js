e46c563e2597a9546706dfca6ac1cd4a451d4b58
Sam Washington <spwashi2@illinois.edu>
2017-06-10 22:16:04 -0500

    SmJS-74 Throw an error when trying to configure DataSource type with a value that has not already been set     - Integrated changes

--- src/SmJS/_config/index.js
+++ src/SmJS/_config/index.js
@@ -2,44 +2,16 @@
  * Created by Sam Washington on 4/23/17.
  */
 import * as _defaults from "./_defaults";
-export let types   = [..._defaults.types];
-export let models  = Object.assign({},
-                                   _defaults.models,
-                                   {
-                                       _sm_entity:       {
-                                           inherits: ['[Model]_entity'],
-                                           source:   '_'
-                                       },
-                                       _sm_relationship: {
-                                           inherits: ['[Model]_sm_entity', '[Model]_relationship']
-                                       },
-    
-                                       entities:                  {inherits: ['[Model]_sm_entity'], properties: {name: {inherits: ['[Model]_{item_name}']}}},
-                                       entity_classes:            {inherits: ['[Model]_sm_entity']},
-                                       properties:                {
-                                           inherits:   ['[Model]_sm_entity'],
-                                           properties: {value: {type: 'INT'}}
-                                       },
-                                       types:                     {
-                                           inherits:   ['[Model]_sm_entity'],
-                                           properties: {parent_type: {inherits: ['[Model]types|id',]}}
-                                       },
-                                       entity_class_property_map: {
-                                           inherits:   ['[Model]_sm_relationship'],
-                                           properties: {
-                                               entity_class_id: {inherits: ['[Model]entity_classes|id',]},
-                                               property_id:     {inherits: ['[Model]properties|id',]}
-                                           }
-                                       },
-                                       property_type_map:         {
-                                           inherits:   ['[Model]_sm_relationship'],
-                                           properties: {
-                                               property_id: {inherits: ['[Model]properties|id',]},
-                                               type_id:     {inherits: ['[Model]types|id',]}
-                                           }
-                                       }
-                                   });
-export let sources = {
+import DataSource from "../entities/DataSource";
+export let types = [..._defaults.types];
+
+DataSource.acceptedTypes = {
+    database: {},
+    db_table: {},
+    JSON:     {}
+};
+
+export let dataSources = {
     _factshift_config: {
         name:    'FactshiftConfig',
         type:    'database',
@@ -51,5 +23,40 @@ export let sources = {
         details: {database: '[source|_factshift_config]'}
     }
 };
-
-export default {models, sources};
+export let models      = Object.assign({},
+                                       _defaults.models,
+                                       {
+                                           _sm_entity:       {
+                                               inherits: ['[Model]_entity'],
+                                               source:   '_'
+                                           },
+                                           _sm_relationship: {
+                                               inherits: ['[Model]_sm_entity', '[Model]_relationship']
+                                           },
+    
+                                           entities:                  {inherits: ['[Model]_sm_entity'], properties: {name: {inherits: ['[Model]_{item_name}']}}},
+                                           entity_classes:            {inherits: ['[Model]_sm_entity']},
+                                           properties:                {
+                                               inherits:   ['[Model]_sm_entity'],
+                                               properties: {value: {type: 'INT'}}
+                                           },
+                                           types:                     {
+                                               inherits:   ['[Model]_sm_entity'],
+                                               properties: {parent_type: {inherits: ['[Model]types|id',]}}
+                                           },
+                                           entity_class_property_map: {
+                                               inherits:   ['[Model]_sm_relationship'],
+                                               properties: {
+                                                   entity_class_id: {inherits: ['[Model]entity_classes|id',]},
+                                                   property_id:     {inherits: ['[Model]properties|id',]}
+                                               }
+                                           },
+                                           property_type_map:         {
+                                               inherits:   ['[Model]_sm_relationship'],
+                                               properties: {
+                                                   property_id: {inherits: ['[Model]properties|id',]},
+                                                   type_id:     {inherits: ['[Model]types|id',]}
+                                               }
+                                           }
+                                       });
+export default {models, dataSources};

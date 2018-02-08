f500288c550fa2c0d6a523c6116ccf7c5c1cba60
Sam Washington <spwashi2@illinois.edu>
2018-02-07 21:13:36 -0600

    Minor change to application

--- src/components/sm/application/application.js
+++ src/components/sm/application/application.js
@@ -2,11 +2,10 @@ import {Configuration, EVENT__CONFIG} from "../../configuration/configuration";
 import ModelConfiguration from "../entities/model/configuration";
 import {Model} from "../entities/model/model";
 import {Configurable} from "../../configuration/types";
-import {ITEM_CONFIGURED__EVENT} from "../entities/model/events";
 
 export class ApplicationConfiguration extends Configuration {
     handlers = {
-        models: (modelConfigObj, owner) => {
+        models:    (modelConfigObj, owner) => {
             const config          = modelConfigObj;
             const allInitializing = Object.keys(config)
                                           .map(key => {
@@ -21,19 +20,41 @@ export class ApplicationConfiguration extends Configuration {
                                           });
             return Promise.all(allInitializing)
         },
-        routes: (routes, owner) => {
+        routes:    (routes, owner) => {
             return owner._routes = routes || {};
-        }
+        },
+        name:      (name, app) => app._name = name,
+        namespace: (namespace, app) => app._namespace = namespace,
+        domain:    (domain, app) => app._domain = domain,
+        urlPath:   (urlPath, app) => {
+            if (typeof urlPath !== "string" || !urlPath.length) return;
+        
+            return app._urlPath = urlPath;
+        },
     }
 }
 
 export class Application implements Configurable {
     _models: {};
     _routes: {};
+    _name: string;
+    _namespace: string;
+    _urlPath: string;
+    _domain: string;
     
     constructor() {
         this._models = {};
     }
+    
+    get name() {return this._name}
+    
+    get namespace() {return this._namespace}
+    
+    get urlPath() {return this._urlPath}
+    
+    get domain() {return this._domain}
+    
+    get url() {return this._domain + (this.urlPath ? `/${this.urlPath}/` : '')}
     
     get models() {
         return this._models;
@@ -44,10 +65,14 @@ export class Application implements Configurable {
     }
     
     toJSON() {
-        return {
-            models: this._models,
-            routes: this._routes
-    
-        }
+        const obj = {};
+        this._name && (obj.name = this._name);
+        this._namespace && (obj.namespace = this._namespace);
+        this._routes && (obj.routes = this._routes);
+        this._models && (obj.models = this._models);
+        this._urlPath && (obj.urlPath = this._urlPath);
+        this._domain && (obj.domain = this._domain);
+        this._domain && (obj.url = this.url);
+        return obj
     }
 }
--- src/components/sm/index.js
+++ src/components/sm/index.js
@@ -2,5 +2,6 @@ import {Entity} from "./entities/entity/entity";
 import {Property} from "./entities/property/property";
 import {Model} from "./entities/model/model";
 import * as identification from './identification'
+import {Application, ApplicationConfiguration} from "./application/application";
 
-export {Entity, Property, Model, identification}
+export {Entity, Property, Model, identification, Application, ApplicationConfiguration}
